2dcdd1b3ff3d8bfbff64cb31d3a04feb4b57ad8c
esmeevanweenen <esmeevanweenen1995@gmail.com>
2023-01-23 13:48:26 +0100

    Opdracht 3 gemaakt

--- loops.js
+++ loops.js
@@ -41,7 +41,13 @@ for (let i = 0; i < 5; i++) {
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
-
+for (let i = 0; i < 16; i++) {
+    if ((i % 2) === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is oneven");
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
